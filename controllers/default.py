fcefd2cda440709bba14ffbc95d330beff6eed4f
foodcrazyjim <sanjit.samaddar@gmail.com>
2016-03-06 01:16:09 +0000

    Removed modal for now.  Can now add items to wishlist and have list.
    Also can now add item without image.

--- controllers/default.py
+++ controllers/default.py
@@ -17,7 +17,7 @@ def index():
     return auth.wiki()
     """
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'),form=auth())
+    return dict(message=T('Welcome to web2py!'))
 
 
 def user():
@@ -36,10 +36,10 @@ def user():
         @auth.requires_permission('read','table name',record_id)
     to decorate functions that need access control
     """
-    return dict(form=auth())
+    return dict()
 
 def login_modal():
-    return dict(form=auth())
+    return dict()
 
 @cache.action()
 def download():
@@ -65,10 +65,10 @@ def collections():
                     items = db((db.item.ownedBy == auth.user.id)).select())
     else:
         return dict(collections= db((db.collection.id > 0) & (db.collection.name != "Have List")& (db.collection.name != "Want List")).select(),
-                    items = db((db.item.id > 0)).select(),form=auth())
+                    items = db((db.item.id > 0)).select())
 
 def collection():
-    return dict(items =db(db.item.inCollection.contains(request.args(0))).select(), collection = db.collection(request.args(0)),form=auth())
+    return dict(items =db(db.item.inCollection.contains(request.args(0))).select(), collection = db.collection(request.args(0)))
 
 def new_collection():
     addform = FORM(DIV(LABEL('Name*', _for='product_name',_class="checkbox col-xs-12")),
@@ -83,7 +83,7 @@ def new_collection():
         response.flash = 'One or more of your form fields has an error. Please see below for more information'
     else:
         response.flash = 'Please complete the form below to add a new product.'
-    return dict(addform=addform,form=auth())
+    return dict(addform=addform)
 
 def edit_collection():
     record = db.collection(request.args(0))
@@ -98,7 +98,7 @@ def edit_collection():
         response.flash = 'One or more of your form fields has an error. Please see below for more information'
     else:
         response.flash = 'Please complete the form below to add a new product.'
-    return dict(updateform=updateform, collection=record,form=auth())
+    return dict(updateform=updateform, collection=record)
 def add_to_collection():
     record = db.collection(request.args(0))
     inCollectionList=[record.id]
@@ -114,7 +114,7 @@ def add_to_collection():
                           'Film and Television','Glass and Pottery','Household Items','Memorabilia','Music','Nature and Animals','Sports','Technology',
                           'Themed','Toys and Games','Miscellaneous', value='Miscellaneous',_name='type',_class="form-control")),
                BR(),
-               DIV(LABEL(INPUT(_name='have_list',_type="checkbox", _checked=record.private),'Add to have list'),_class="small_margins checkbox"),
+               DIV(LABEL(INPUT(_name='have_list',_type="checkbox"),'Add to have list'),_class="small_margins checkbox"),
               _class='form-group col-xs-6'),
                DIV(
                DIV(LABEL('Image*', _for='product_name')),
@@ -129,7 +129,12 @@ def add_to_collection():
         if request.vars.have_list:
             myHaveList = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Have List")).select()
             inCollectionList.append(myHaveList[0].id)
-        image = db.item.image.store(request.vars.image.file,request.vars.image.filename)
+        try:
+            len(request.vars.image) #This means image not uploaded
+            image = None
+        except TypeError:
+            #Image has been uploaded (as the exception occured); update image in db
+            image = db.item.image.store(request.vars.image.file,request.vars.image.filename)
         db.item.insert(name=request.vars.name,price=request.vars.value,type=request.vars.type,description=request.vars.description,
                        inCollection=inCollectionList,ownedBy=auth.user.id,image=image)
         db.commit
@@ -138,30 +143,108 @@ def add_to_collection():
         response.flash = 'One or more of your form fields has an error. Please see below for more information'
     else:
         response.flash = 'Please complete the form below to add a new product.'
-    return dict(addform=addform, collection=record,form=auth())
+    return dict(addform=addform, collection=record)
 
 def wishlist():
     myWishList = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Want List")).select()
-    return dict(items = db((db.item.inCollection.contains(myWishList[0].id))).select(),form=auth())
+    return dict(items = db((db.item.inCollection.contains(myWishList[0].id))).select())
 
 def have_list():
     myHaveList = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Have List")).select()
-    return dict(items = db((db.item.inCollection.contains(myHaveList[0].id))).select(),form=auth())
+    return dict(items = db((db.item.inCollection.contains(myHaveList[0].id))).select())
 
 def edit_wishlist_item():
-    return dict(form=auth())
+    return dict()
 
 def add_to_wishlist():
-    return dict(form=auth())
+    record = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Want List")).select()[0]
+    inCollectionList=[record.id]
+    addform = FORM(DIV(
+               DIV(LABEL('Name*', _for='product_name')),
+               DIV(INPUT(_name='name',_placeholder = "Name of item...",requires=IS_NOT_EMPTY(),_class="form-control")),
+               BR(),
+               DIV(LABEL('Value*', _for='product_value')),
+               DIV(INPUT(_name='value',_placeholder = "Value of item...",requires=IS_NOT_EMPTY() and IS_INT_IN_RANGE(0,9999),_class="form-control")),
+               BR(),
+               DIV(LABEL('Type*', _for='product_type')),
+               DIV(SELECT('Advertising and Brand','Architectural','Art','Books,Magazines and Paper','Clothing,Fabric and Textiles','Coins,Currency,Stamps',
+                          'Film and Television','Glass and Pottery','Household Items','Memorabilia','Music','Nature and Animals','Sports','Technology',
+                          'Themed','Toys and Games','Miscellaneous', value='Miscellaneous',_name='type',_class="form-control")),
+              _class='form-group col-xs-6'),
+               DIV(
+               DIV(LABEL('Image*', _for='product_name')),
+               DIV(INPUT(_name='image',_type='file')),
+               BR(),
+               DIV(LABEL('Description', _for='product_name')),
+               DIV(TEXTAREA(_name='description',_class='form-control',_rows='8',_placeholder='Please enter item description')),
+               BR(),
+               DIV(INPUT(_type='submit', _value='Submit', _class="form-control btn btn-primary")),
+                   _class='form-group col-xs-6'),_class="small_margins")
+    if addform.accepts(request,session):
+        try:
+            len(request.vars.image) #This means image not uploaded
+            image = None
+        except TypeError:
+            comic_data.update_record(image=request.vars.image) #Image has been uploaded (as the exception occured); update image in db
+            image = db.item.image.store(request.vars.image.file,request.vars.image.filename)
+        db.item.insert(name=request.vars.name,price=request.vars.value,type=request.vars.type,description=request.vars.description,
+                       inCollection=inCollectionList,ownedBy=auth.user.id,image=image)
+        db.commit
+        redirect(URL('default','wishlist'))
+    elif addform.errors:
+        response.flash = 'One or more of your form fields has an error. Please see below for more information'
+    else:
+        response.flash = 'Please complete the form below to add a new product.'
+    return dict(addform=addform, collection=record)
 
+def add_to_havelist():
+    record = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Have List")).select()[0]
+    inCollectionList=[record.id]
+    addform = FORM(DIV(
+               DIV(LABEL('Name*', _for='product_name')),
+               DIV(INPUT(_name='name',_placeholder = "Name of item...",requires=IS_NOT_EMPTY(),_class="form-control")),
+               BR(),
+               DIV(LABEL('Value*', _for='product_value')),
+               DIV(INPUT(_name='value',_placeholder = "Value of item...",requires=IS_NOT_EMPTY() and IS_INT_IN_RANGE(0,9999),_class="form-control")),
+               BR(),
+               DIV(LABEL('Type*', _for='product_type')),
+               DIV(SELECT('Advertising and Brand','Architectural','Art','Books,Magazines and Paper','Clothing,Fabric and Textiles','Coins,Currency,Stamps',
+                          'Film and Television','Glass and Pottery','Household Items','Memorabilia','Music','Nature and Animals','Sports','Technology',
+                          'Themed','Toys and Games','Miscellaneous', value='Miscellaneous',_name='type',_class="form-control")),
+              _class='form-group col-xs-6'),
+               DIV(
+               DIV(LABEL('Image*', _for='product_name')),
+               DIV(INPUT(_name='image',_type='file')),
+               BR(),
+               DIV(LABEL('Description', _for='product_name')),
+               DIV(TEXTAREA(_name='description',_class='form-control',_rows='8',_placeholder='Please enter item description')),
+               BR(),
+               DIV(INPUT(_type='submit', _value='Submit', _class="form-control btn btn-primary")),
+                   _class='form-group col-xs-6'),_class="small_margins")
+    if addform.accepts(request,session):
+        try:
+            len(request.vars.image) #This means image not uploaded
+            image = None
+        except TypeError:
+            comic_data.update_record(image=request.vars.image) #Image has been uploaded (as the exception occured); update image in db
+            image = db.item.image.store(request.vars.image.file,request.vars.image.filename)
+        db.item.insert(name=request.vars.name,price=request.vars.value,type=request.vars.type,description=request.vars.description,
+                       inCollection=inCollectionList,ownedBy=auth.user.id,image=image)
+        db.commit
+        redirect(URL('default','wishlist'))
+    elif addform.errors:
+        response.flash = 'One or more of your form fields has an error. Please see below for more information'
+    else:
+        response.flash = 'Please complete the form below to add a new product.'
+    return dict(addform=addform, collection=record)
 def advanced_search():
-    return dict(form=auth())
+    return dict()
 
 def trade():
-    return dict(form=auth())
+    return dict()
 
 def trade_history():
-    return dict(form=auth())
+    return dict()
 
 def item_info_by_id():
     info = db(db.item.id == request.get_vars.id).select()
@@ -174,4 +257,17 @@ def item_info_by_id():
     else:
         is_tradable = False
     ## NEED is_tradable = True/False
-    return dict(info = info, owner= owner, is_tradable = is_tradable)
+
+
+    myHaveList = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Have List")).select()
+    if (info[0].ownedBy == auth.user.id) and (myHaveList[0].id not in info[0].inCollection):
+        have_list_ok = True
+    else:
+        have_list_ok = False
+
+    myWishList = db((db.collection.ownedBy == auth.user.id) & (db.collection.name == "Want List")).select()
+    if (info[0].ownedBy != auth.user.id) and (myWishList[0].id not in info[0].inCollection):
+        wishlist_ok = True
+    else:
+        wishlist_ok = False
+    return dict(info = info, owner= owner, is_tradable = is_tradable, have_list_ok = have_list_ok, wishlist_ok = wishlist_ok)
